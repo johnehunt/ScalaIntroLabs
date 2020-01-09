1814c378df54b71a160beaaa9d87ca50191f0ecb
John Hunt <john@matraining.com>
2020-01-09 13:11:58 +0000

    Added comments and some reformatting

--- 04-case-classes/src/bookshop/BookshopApp.scala
+++ 04-case-classes/src/bookshop/BookshopApp.scala
@@ -4,4 +4,6 @@ object BookshopApp extends App {
   println("Welcome to the Bookshop\n")
 
   Bookshop.printDetails()
+
+  print("Done")
 }
--- 04-case-classes/src/bookshop/Genre.scala
+++ 04-case-classes/src/bookshop/Genre.scala
@@ -1,5 +1,9 @@
 package bookshop
 
+/**
+ * Case objects representing different Genres.
+ */
+
 case object Fiction
 case object Technical
 case object History
