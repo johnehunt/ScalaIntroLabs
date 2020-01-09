49937fa0a8ebb8dcd76e1cc041a28dbe1091f7fa
John Hunt <john@matraining.com>
2020-01-09 13:08:48 +0000

    Added comments and some reformatting

--- 03-bookshop-object/src/bookshop/Bookshop.scala
+++ 03-bookshop-object/src/bookshop/Bookshop.scala
@@ -1,18 +1,24 @@
 package bookshop
 
+/**
+ * Bookshop Repository Singleton object - used to hold books for the bookshop.
+ */
 object BookshopRepository {
   def getBooks(): Book = {
     val author = new Author("Pete Smith")
     val address = new Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
     val publisher = new Publisher("Tech Books Publishing Ltd.", address)
     val book = new Book("Scala Unleashed", 15.95, author, publisher)
-    return book
+    book
   }
 }
 
+/**
+ * Singleton object representing the Bookshop.
+ */
 object Bookshop {
-  val name = "Scala Books of the World"
-  val address = new Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
+  val name: String = "Scala Books of the World"
+  val address: Address = new Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
   val books = BookshopRepository.getBooks()
 
   def printDetails(): Unit = {
