3ff6a1c50fea98f89d1e47d0bafb5996a720948f
John Hunt <john@matraining.com>
2020-01-09 13:18:23 +0000

    Fixed bug

--- 07-traits/src/bookshop/Book.scala
+++ 07-traits/src/bookshop/Book.scala
@@ -2,12 +2,14 @@ package bookshop
 
 trait Sales {
   val price: Price // Abstract property price
-  private var salePercentage = 0.0;
+  private var salePercentage = 0.0
+
   def setSaleDiscount(salePercentage: Double): Unit = {
     this.salePercentage = salePercentage / 100
   }
+
   def calculateSalePrice(): Price = {
-    return price - (price.value * salePercentage)
+    price - (price.value * salePercentage)
   }
 }
 
@@ -23,14 +25,19 @@ abstract class Entity(name: String)
 
 case class Author(_name: String) extends Entity(_name)
 
-case class Publisher(_name: String, val address: Address) extends Entity(_name)
+case class Publisher(_name: String, address: Address) extends Entity(_name)
 
-case class Address(val number: Int, val street: String, val city: String, val county: String, val postcode: String)
+case class Address(number: Int, street: String, city: String, county: String, postcode: String)
 
-case class Price(value: Double) extends AnyVal  {
+case class Price(value: Double) extends AnyVal {
   def +(d: Double): Price = Price(value + d)
+
   def -(d: Double): Price = Price(value - d)
+
   def *(d: Double): Price = Price(value * d)
+
   def /(d: Double): Price = Price(value / d)
+
+  def print(): Unit = println(s"Price($value)")
 }
 
--- 07-traits/src/bookshop/Genre.scala
+++ 07-traits/src/bookshop/Genre.scala
@@ -1,5 +1,8 @@
 package bookshop
 
+/**
+ * Provide a root abstract class for the case objects.
+ */
 abstract class Genre
 
 case object Fiction extends Genre
