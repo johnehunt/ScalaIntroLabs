3aa5ee68a19a8d1d9ddcdd69250682f80ac064a5
John Hunt <john@matraining.com>
2020-03-03 11:17:33 +0000

    Tidied Value Type implementation

--- 05-case-classes/src/bookshop/Book.scala
+++ 05-case-classes/src/bookshop/Book.scala
@@ -1,15 +1,17 @@
 package bookshop
 
-class Book(val title: String, val price: Price, val author: Author, val publisher: Publisher, val genre: AnyRef) {
+class Book(val title: String,
+           val price: Price,
+           val author: Author,
+           val publisher: Publisher,
+           val genre: AnyRef) {
   private var salePercentage = 0.0
 
   def setSaleDiscount(salePercentage: Double): Unit = {
     this.salePercentage = salePercentage / 100
   }
 
-  def calculateSalePrice(): Price = {
-    price - (price.value * salePercentage)
-  }
+  def calculateSalePrice(): Price = price - (price * salePercentage)
 
   override def toString: String = s"Book($title, $price, $author, Genre: $genre, \n$publisher)"
 }
@@ -18,12 +20,16 @@ case class Author(name: String)
 
 case class Publisher(name: String, address: Address)
 
-case class Address(number: Int, street: String, city: String, county: String, postcode: String)
+case class Address(number: Int,
+                   street: String,
+                   city: String,
+                   county: String,
+                   postcode: String)
 
 case class Price(value: Double) extends AnyVal {
-  def +(d: Double): Price = Price(value + d)
+  def +(p: Price): Price = Price(value + p.value)
 
-  def -(d: Double): Price = Price(value - d)
+  def -(p: Price): Price = Price(value - p.value)
 
   def *(d: Double): Price = Price(value * d)
 
