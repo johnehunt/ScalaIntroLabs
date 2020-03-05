bead8877a792e3fba8174f110c8213fade474d0f
John Hunt <john@matraining.com>
2020-03-05 14:27:04 +0000

    Refactored directory structure

--- 14-testing/src/test/scala/specs/ShoppingCartAcceptanceSpec.scala
+++ 14-testing/src/test/scala/specs/ShoppingCartAcceptanceSpec.scala
@@ -11,7 +11,7 @@ class ShoppingCartAcceptanceSpec extends Specification { def is = s2"""
  The Shopping Cart string should
    be empty when first created                                   $e1
    hold one item when added                                      $e2
-   should calculate the                                          $e3
+   should calculate the price of the shopping cart               $e3
                                                                  """
   def e1 = {
     val cart = new ShoppingCart()
