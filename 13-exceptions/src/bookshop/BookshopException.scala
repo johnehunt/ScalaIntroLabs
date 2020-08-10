5f0da3e185a838beda4ab3b5754abb9a8ac022f9
John Hunt <john.hunt10@gmail.com>
2020-08-10 12:45:49 +0100

    Fixed typo in scaladoc

--- 13-exceptions/src/bookshop/BookshopException.scala
+++ 13-exceptions/src/bookshop/BookshopException.scala
@@ -2,6 +2,6 @@ package bookshop
 
 /**
  * Exception class for use with the Bookshop
- * @param _msg Messaeg ti be displayed with the exception.
+ * @param _msg Message to be displayed with the exception.
  */
 class BookshopException(_msg: String = "Bookshop Error") extends RuntimeException(_msg)
