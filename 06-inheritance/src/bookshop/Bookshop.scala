73e8423b17f559262c74874c57f9c88d51028c58
John Hunt <john.hunt10@gmail.com>
2020-09-29 12:26:27 +0100

    Removed extra object

--- 06-inheritance/src/bookshop/Bookshop.scala
+++ 06-inheritance/src/bookshop/Bookshop.scala
@@ -1,15 +1,5 @@
 package bookshop
 
-object BookshopRepository {
-  def getBooks(): Book = {
-    val author = Author("Pete Smith")
-    val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
-    val publisher = Publisher("Tech Books Publishing Ltd.", address)
-    val book = new Book("Scala Unleashed", Price(15.95), author, publisher, Technical)
-    book
-  }
-}
-
 object Bookshop {
   val name: String = "Scala Books of the World"
   val address: Address = Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
@@ -19,6 +9,13 @@ object Bookshop {
     println(s"name: $name")
     println(s"address: $address")
     println(s"Books: $books")
+  }
+
+  def getBooks(): Book = {
+    val author = Author("Pete Smith")
+    val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
+    val publisher = Publisher("Tech Books Publishing Ltd.", address)
+    new Book("Scala Unleashed", Price(15.95), author, publisher, Technical)
   }
 
 }
