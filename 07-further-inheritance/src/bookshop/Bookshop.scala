690c1f898c55f2d322ff0a98977847e347b3f8aa
John Hunt <john.hunt10@gmail.com>
2020-09-29 15:32:19 +0100

    Layout change

--- 07-further-inheritance/src/bookshop/Bookshop.scala
+++ 07-further-inheritance/src/bookshop/Bookshop.scala
@@ -1,24 +1,22 @@
 package bookshop
 
-object BookshopRepository {
+object Bookshop {
+  val name: String = "Scala Books of the World"
+  val address: Address = Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
+  val books: Book = getBooks()
+
+  def printDetails(): Unit = {
+    println(s"name: $name")
+    println(s"address: $address")
+    println(s"Books: $books")
+  }
+
   def getBooks(): Book = {
     val author = Author("Pete Smith")
     val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
     val publisher = Publisher("Tech Books Publishing Ltd.", address)
     val book = new Book("Scala Unleashed", Price(15.95), author, publisher, Technical)
     book
-  }
-}
-
-object Bookshop {
-  val name: String = "Scala Books of the World"
-  val address: Address = Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
-  val books: Book = BookshopRepository.getBooks()
-
-  def printDetails(): Unit = {
-    println(s"name: $name")
-    println(s"address: $address")
-    println(s"Books: $books")
   }
 
 }
