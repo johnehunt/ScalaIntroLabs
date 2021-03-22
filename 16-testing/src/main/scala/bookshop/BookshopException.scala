f84dc8045201ce0e9e10ae9b86ef15f360cace74
John Hunt <john.hunt10@gmail.com>
2021-03-22 14:04:17 +0000

    Removed unused classes

--- 16-testing/src/main/scala/bookshop/BookshopException.scala
+++
@@ -1,7 +0,0 @@
-package bookshop
-
-/**
- * Exception class for use with the Bookshop
- * @param _msg Message to  be displayed with the exception.
- */
-class BookshopException(_msg: String = "Bookshop Error") extends RuntimeException(_msg)
--- 16-testing/src/main/scala/bookshop/Genre.scala
+++
@@ -1,9 +0,0 @@
-package bookshop
-
-abstract class Genre
-
-case object Fiction extends Genre
-case object Technical extends Genre
-case object History extends Genre
-case object Detective extends Genre
-case object Biography extends Genre
