46564f32087be4074117f2c50c71e239bff7dd13
John Hunt <john.hunt10@gmail.com>
2021-03-22 11:42:04 +0000

    Changed name of method and made private

--- 08-further-inheritance/src/bookshop/Bookshop.scala
+++ 08-further-inheritance/src/bookshop/Bookshop.scala
@@ -3,7 +3,7 @@ package bookshop
 object Bookshop {
   val name: String = "Scala Books of the World"
   val address: Address = Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
-  val book: Book = getBook()
+  val book: Book = setupBook()
 
   def printDetails(): Unit = {
     println(s"name: $name")
@@ -11,7 +11,7 @@ object Bookshop {
     println(s"Book: $book")
   }
 
-  def getBook(): Book = {
+  private def setupBook(): Book = {
     val author = Author("Pete Smith")
     val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
     val publisher = Publisher("Tech Books Publishing Ltd.", address)
