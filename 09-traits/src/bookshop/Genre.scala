467970366efd16fc6e071e0b8a22766983881fd0
John Hunt <john.hunt10@gmail.com>
2021-03-22 12:09:13 +0000

    Return to an abstract class

--- 09-traits/src/bookshop/Genre.scala
+++ 09-traits/src/bookshop/Genre.scala
@@ -3,7 +3,7 @@ package bookshop
 /**
  * Provide a root trait for the case objects.
  */
-sealed trait Genre
+abstract class Genre
 
 case object Fiction extends Genre
 case object Technical extends Genre
