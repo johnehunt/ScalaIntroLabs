f3a0b9c17fb135c3b775c69a47d9cdee8db554ce
John Hunt <john.hunt10@gmail.com>
2021-03-22 10:55:55 +0000

    Minor layout changes

--- 04-bookshop-object/src/bookshop/Book.scala
+++ 04-bookshop-object/src/bookshop/Book.scala
@@ -1,13 +1,19 @@
 package bookshop
 
-class Book(val title: String, val price: Double, val author: Author, val publisher: Publisher) {
-  private var salePercentage = 0.0
-  def setSaleDiscount(salePercentage: Double): Unit = {
+// Classes for the book
+
+class Book(val title: String,
+           var price: Double,
+           val author: Author,
+           val publisher: Publisher) {
+
+  private var salePercentage: Double = 0.0
+
+  def setSaleDiscount(salePercentage: Double): Unit =
     this.salePercentage = salePercentage / 100
-  }
-  def calculateSalePrice(): Double = {
-    price - (price * salePercentage)
-  }
+
+  def calculateSalePrice(): Double = price - (price * salePercentage)
+
   override def toString: String = s"Book($title, $price, $author, \n$publisher)"
 }
 
@@ -15,11 +21,15 @@ class Author(val name: String) {
   override def toString: String = name
 }
 
-class Publisher(val name: String, val address: Address) {
+class Publisher(val name: String,
+                var address: Address) {
   override def toString: String = s"$name,\n$address"
 }
 
-class Address(val number: Int, val street: String, val city: String, val county: String, val postcode: String) {
+class Address(val number: Int,
+              val street: String,
+              val city: String,
+              val county: String,
+              val postcode: String) {
   override def toString: String = s"$number $street,\n$city,\n$county,\n$postcode"
 }
-
--- 04-bookshop-object/src/bookshop/Bookshop.scala
+++ 04-bookshop-object/src/bookshop/Bookshop.scala
@@ -4,17 +4,18 @@ package bookshop
  * Singleton object representing the Bookshop.
  */
 object Bookshop {
+
   val name: String = "Scala Books of the World"
   val address: Address = new Address(26, "Main Street", "Bath", "BANES", "BA1 3ZZ")
-  val books: Book = getBooks()
+  val book: Book = setupBook()
 
   def printDetails(): Unit = {
     println(s"name: $name")
     println(s"address: $address")
-    println(s"Books: $books")
+    println(s"Book: $book")
   }
 
-  private def getBooks(): Book = {
+  private def setupBook(): Book = {
     val author = new Author("Pete Smith")
     val address = new Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
     val publisher = new Publisher("Tech Books Publishing Ltd.", address)
