890c152a92c0090717ebc28c3042c4d39093a3bc
John Hunt <john.hunt10@gmail.com>
2021-03-22 10:37:19 +0000

    Slightly extended lab solution

--- 02-hello-bookshop/src/BookshopApp.scala
+++ 02-hello-bookshop/src/BookshopApp.scala
@@ -3,4 +3,12 @@
  */
 object BookshopApp extends App {
   println("Welcome to the Bookshop")
+
+  val message = "We have a lot of offers today"
+  var day = "Monday"
+
+  println(s"$day $message")
+
+  day = "Tuesday"
+  println(s"$day - $message")
 }
