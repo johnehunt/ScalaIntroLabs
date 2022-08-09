0e96ae071041e7fd0e9799d4707517d8369c8856
John Hunt <john.hunt10@gmail.com>
2022-08-09 08:52:23 +0100

    Updates to equals and hashcodes

--- 05-options/src/bookshop/Book.scala
+++ 05-options/src/bookshop/Book.scala
@@ -19,7 +19,6 @@ class Book(val title: String,
 
   override def toString(): String = s"Book($title, $price, ${author.getOrElse("Anonymous")}, \n$publisher)"
 
-
   override def equals(other: Any): Boolean = other match {
     case that: Book =>
       title == that.title &&
@@ -29,18 +28,44 @@ class Book(val title: String,
   }
 
   override def hashCode(): Int = {
-    val state = Seq(title, author, publisher)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    title.hashCode() + author.hashCode() + publisher.hashCode()
   }
 }
 
 class Author(val name: String) {
   override def toString: String = name
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[Author]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Author =>
+      (that canEqual this) &&
+        name == that.name
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    name.hashCode()
+  }
 }
 
 class Publisher(val name: String,
                 var address: Address) {
   override def toString(): String = s"$name,\n$address"
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[Publisher]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Publisher =>
+      (that canEqual this) &&
+        name == that.name &&
+        address == that.address
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    name.hashCode() + address.hashCode()
+  }
 }
 
 class Address(val number: Int,
@@ -49,4 +74,22 @@ class Address(val number: Int,
               val county: String,
               val postcode: String) {
   override def toString: String = s"$number $street,\n$city,\n$county,\n$postcode"
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[Address]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Address =>
+      (that canEqual this) &&
+        number == that.number &&
+        street == that.street &&
+        city == that.city &&
+        county == that.county &&
+        postcode == that.postcode
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(number, street, city, county, postcode)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
 }
