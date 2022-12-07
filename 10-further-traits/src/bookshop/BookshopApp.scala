f3c370f83a8bcde12c7af78f43445ee751ca8a83
John Hunt <john.hunt10@gmail.com>
2022-12-07 10:39:47 +0000

    Added services

--- 10-further-traits/src/bookshop/BookshopApp.scala
+++ 10-further-traits/src/bookshop/BookshopApp.scala
@@ -7,5 +7,14 @@ object BookshopApp extends App {
 
   Bookshop.book.price.print()
 
+  println("-" * 25)
+
+  val s1 = newService("books", Price(125.55), "Smiths")
+  println(s1)
+  s1.setSaleDiscount(10.0)
+  println(s1.calculateSalePrice())
+  println(s"Maintenance day - ${s1.maintenance}")
+
+
   println("Done")
 }
---
+++ 10-further-traits/src/bookshop/Service.scala
@@ -0,0 +1,22 @@
+import DaysOfWeek._
+
+class Service(val title: String,
+              val price: Price,
+              val provider: String,
+              val maintenance: WeekDay=Sunday) extends Sales {
+
+  override def toString = s"Game($provider) - ${super.toString()}"
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Service =>
+        title == that.title &&
+        price == that.price &&
+          provider == that.provider
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    title.hashCode() + price.hashCode() + provider.hashCode()
+  }
+
+}
