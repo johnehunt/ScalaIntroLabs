04f67d14d87b098448acbada8fe66f2104e9ba08
John Hunt <john.hunt10@gmail.com>
2023-11-14 16:50:29 +0000

    Simplified

--- 10-further-traits/src/bookshop/BookshopApp.scala
+++ 10-further-traits/src/bookshop/BookshopApp.scala
@@ -9,7 +9,7 @@ object BookshopApp extends App {
 
   println("-" * 25)
 
-  val s1 = newService("books", Price(125.55), "Smiths")
+  val s1 = Service("books", Price(125.55), "Smiths")
   println(s1)
   s1.setSaleDiscount(10.0)
   println(s1.calculateSalePrice())
--- 10-further-traits/src/bookshop/Service.scala
+++ 10-further-traits/src/bookshop/Service.scala
@@ -1,22 +1,8 @@
 import DaysOfWeek._
 
-class Service(val title: String,
-              val price: Price,
-              val provider: String,
-              val maintenance: WeekDay=Sunday) extends Sales {
-
-  override def toString = s"Service($provider) - ${super.toString()}"
-
-  override def equals(other: Any): Boolean = other match {
-    case that: Service =>
-        title == that.title &&
-        price == that.price &&
-          provider == that.provider
-    case _ => false
-  }
-
-  override def hashCode(): Int = {
-    title.hashCode() + price.hashCode() + provider.hashCode()
-  }
+case class Service( title: String,
+                    price: Price,
+                    provider: String,
+                    maintenance: Value = Sunday) extends Sales {
 
 }
