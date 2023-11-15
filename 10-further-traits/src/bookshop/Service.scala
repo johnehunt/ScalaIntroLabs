4b1ee7a737f2d74337f65a0f9cecb91ddcdf840a
John Hunt <john.hunt10@gmail.com>
2023-11-15 10:54:39 +0000

    Updated example

--- 10-further-traits/src/bookshop/Service.scala
+++ 10-further-traits/src/bookshop/Service.scala
@@ -1,8 +1,44 @@
 import DaysOfWeek._
 
-case class Service( title: String,
-                    price: Price,
-                    provider: String,
-                    maintenance: Value = Sunday) extends Sales {
+class Service private(val title: String,
+                      val price: Price,
+                      val provider: String,
+                      val maintenance: Value) extends Sales {
+
+
+  override def toString = s"Service($title, $price, $provider, $maintenance)"
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Service =>
+      title == that.title &&
+        price == that.price &&
+        provider == that.provider &&
+        maintenance == that.maintenance
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    title.hashCode() + price.hashCode() + provider.hashCode() + maintenance.hashCode()
+  }
+
+  def copy(copyTitle: Option[String] = None,
+           copyPrice: Option[Price] = None,
+           copyProvider: Option[String] = None,
+           copyMaintenance: Option[Value] = None): Service = {
+    val newTitle = copyTitle.getOrElse(this.title)
+    val newPrice = copyPrice.getOrElse(this.price)
+    val newProvider = copyProvider.getOrElse(this.provider)
+    val newMaintenance = copyMaintenance.getOrElse(this.maintenance)
+    new Service(newTitle, newPrice, newProvider, newMaintenance)
+  }
 
 }
+
+object Service {
+  def apply(title: String,
+            price: Price,
+            provider: String,
+            maintenance: Value = Sunday): Service =
+    new Service(title, price, provider, maintenance)
+}
+
