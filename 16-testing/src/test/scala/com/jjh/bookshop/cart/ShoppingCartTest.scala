77c3c3eb753bd04fa8cd424164d84827f2e6acdb
John Hunt <john.hunt10@gmail.com>
2023-11-16 15:17:26 +0000

    Updated example

--- 16-testing/src/test/scala/com/jjh/bookshop/cart/ShoppingCartTest.scala
+++ 16-testing/src/test/scala/com/jjh/bookshop/cart/ShoppingCartTest.scala
@@ -1,11 +1,18 @@
 package com.jjh.bookshop.cart
 
-import bookshop.{Address, Author, Book, Price, Publisher, Technical}
+import com.jjh.bookshop.book.{Address, Author, Book, Publisher, Technical}
+import com.jjh.bookshop.util.Price
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.funspec.AnyFunSpec
 
-class ShoppingCartTest extends AnyFunSpec {
+class ShoppingCartTest extends AnyFunSpec
+  with BeforeAndAfterEach
+  with BeforeAndAfterAll  {
 
   private var cart: Option[ShoppingCart] = None
+
+  override def beforeAll(): Unit = println("Before All")
+  override def afterAll(): Unit = println("After All")
 
   override def beforeEach(): Unit = {
     cart = Some(new ShoppingCart())
@@ -24,32 +31,33 @@ class ShoppingCartTest extends AnyFunSpec {
     describe("when one product added") {
       it("should return size = 1") {
         cart.get.add(getBook())
-        assert(cart.size == 1)
+        assert(cart.get.size == 1)
       }
-      it("should be possible to calculate the total cost") {
+      it("should be possible to calculate the total cost of one book") {
         cart.get.add(getBook())
         val total = cart.get.total()
         assert(total == 15.95)
       }
+      it("should be possible to calculate the total cost of two books") {
+        cart.get.add(getBook())
+        cart.get.add(getBook())
+        val total = cart.get.total()
+        assert(total == 31.9)
+      }
       it("should be possible to remove that product") {
         val book = getBook()
         cart.get.add(book)
+        assert(cart.get.size == 1)
         cart.get.remove(book)
         assert(cart.get.isEmpty())
       }
     }
   }
 
-
-  /**
-   * Private method used to support the tests
-   * in this class.
-   */
   private def getBook(): Book = {
     val author = Author("Pete Smith")
     val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
     val publisher = Publisher("Tech Books Publishing Ltd.", address)
     new Book("Scala Unleashed", Price(15.95), author, publisher, Technical)
   }
-
 }
